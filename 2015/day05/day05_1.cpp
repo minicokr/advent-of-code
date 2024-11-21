74d22416b2545dcd9a988b324417bd4095d76c1f
Min Hee Jo <jom8be@bosch.com>
2024-11-21 21:38:12 +0100

    Solved: 2015 day 5 part 1

---
+++ 2015/day05/day05_1.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <string>
+
+using namespace std;
+
+int main() {
+	long long int result = 0;
+	ifstream input("input.txt");
+
+	static const list<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+	static const list<std::string> not_allowed = {"ab", "cd", "pq", "xy"};
+
+	string s;
+	while (getline(input, s)) {
+		bool satisfied = false;
+
+		// first condition
+		int vowel_cnt = 0;
+
+		for (int i = 0; i < s.size(); i++) {
+			for (const char& v : vowels) {
+				if (s.at(i) == v) {
+					vowel_cnt++;
+				}
+			}	
+		}
+		
+		// bigger than or equal to threshold
+		if (vowel_cnt >= 3) {
+			satisfied = true;
+		}
+
+		// second condition
+		int i;
+		for (i = 0; i < s.size() - 1; i++) {
+			// duplicated character found
+			if (s.at(i) == s.at(i + 1)) {
+				break;
+			}
+		}
+
+		// not found
+		if (i == s.size() - 1) {
+			satisfied = false;
+		}
+
+		// third condition
+		for (const auto& i : not_allowed) {
+			// contains not allowed strings
+			if (s.find(i) != string::npos) {
+				satisfied = false;
+				break;
+			}
+		}
+		
+		// satisfied
+		if (satisfied) {
+			result++;
+		}
+	}
+
+	input.close();
+
+	cout << result << endl;
+
+	return 0;
+}
