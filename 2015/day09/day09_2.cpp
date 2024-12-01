0009a14e4fa8405e2678d13a60bbbe3fec7a7b1b
Min Hee Jo <roxy2131@gmail.com>
2024-12-01 12:54:36 +0100

    Solved: 2015 day 9 part 2

---
+++ 2015/day09/day09_2.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <utility>
+#include <vector>
+#include <climits>
+#include <map>
+#include <set>
+
+#define N 8
+
+using namespace std;
+
+typedef pair<string, string> CityPair;
+typedef pair<CityPair, int> Route;
+typedef vector<Route> RouteVector;
+typedef map<string, bool> Visited;
+
+int getMinimum(const RouteVector& routes, Visited& visited, const string& from, const int& dist, int visitedCount) {
+
+    // all visited
+    if (visitedCount == N) {
+        return dist;
+    }
+
+    int maxDist = 0;
+
+    for (const auto& route : routes) {
+        CityPair citypair = route.first;
+
+        // to exists & still to visit
+        if (citypair.first == from && !visited[citypair.second]) {
+            // visit
+            visited[citypair.second] = true;
+
+            // recursively call next city
+            int temp = getMinimum(routes, visited, citypair.second, dist + route.second, visitedCount + 1);  
+
+            // update maximum 
+            maxDist = max(temp, maxDist);
+
+            // backtrack: unmark the city as visited
+            visited[citypair.second] = false;
+        }
+    }
+
+    return maxDist;
+}
+
+int DFS(const RouteVector& routes, const string& from) {
+    Visited visited;
+
+    // visit the starting point
+    visited[from] = true;
+
+    return getMinimum(routes, visited, from, 0, 1);
+}
+
+int main() {
+    ifstream input("input.txt");
+
+    RouteVector routes;
+    set<string> cities;
+
+    string line;
+    string del_to = " to ";
+    string del_eq = " = ";
+
+    while (getline(input, line)) {
+        // from
+        auto pos = line.find(del_to);
+        string from = line.substr(0, pos);
+		line.erase(0, pos + del_to.length());
+
+        // to
+        pos = line.find(del_eq);
+        string to = line.substr(0, pos);
+        line.erase(0, pos + del_eq.length());
+
+        // distance
+        int dist = stoi(line);
+
+        // add routes
+        routes.push_back({{from, to}, dist});
+        routes.push_back({{to, from}, dist});
+
+        // collect all unique cities
+        cities.insert(from);
+        cities.insert(to);
+    }
+
+    input.close();
+
+    int answer = 0;
+    for (const string& city : cities) {
+        int dist = DFS(routes, city);
+        if (dist != 0) {  // Only consider valid distances
+            answer = max(answer, dist);
+        }
+    }
+
+    cout << answer << endl;
+
+    return 0;
+}
+
