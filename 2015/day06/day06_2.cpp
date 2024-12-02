02ddc47023acc66fae9330c94d7bbfd0d90bab77
Min Hee Jo <roxy2131@gmail.com>
2024-12-02 23:49:02 +0100

    Optimize: 2015 day 6 part 2

--- 2015/day06/day06_2.cpp
+++ 2015/day06/day06_2.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <vector>
 #include <string>
 #include <utility>
-#include <regex>
 #include <numeric>
 
 using namespace std;
@@ -11,34 +11,15 @@ using namespace std;
 typedef vector<vector<int>> Grid;
 typedef pair<int, int> Coordinate;
 
-int m = 1000, n = 1000;
-
-int getInteger(string& s_copy) {
-	int num = 0;
-
-	regex rgx("(\\d+)");
-    smatch match;
-    
-    if (regex_search(s_copy, match, rgx)) {
-		num = stoi(match.str());
-		s_copy = match.suffix();
-    }
-
-	return num;
-}
+const int m = 1000, n = 1000;
 
 void action(Grid& grid, const Coordinate& coord1, const Coordinate& coord2, const string& command) {
-	int coord1x = coord1.first;
-	int coord1y = coord1.second;
-	int coord2x = coord2.first;
-	int coord2y = coord2.second;
-
-	for (int i = coord1x; i <= coord2x; i++) {
-		for (int j = coord1y; j <= coord2y; j++) {
-			if (command == "turn on") {
+	for (int i = coord1.first; i <= coord2.first; i++) {
+		for (int j = coord1.second; j <= coord2.second; j++) {
+			if (command == "on") {
 				grid[i][j]++;
 			}
-			else if (command == "turn off") {
+			else if (command == "off") {
 				if (grid[i][j] > 0) {
 					grid[i][j]--;
 				}
@@ -57,42 +38,33 @@ int countTrueValues(const Grid& grid) {
 }
 
 int main() {
-	long long int result = 0;
 	ifstream input("input.txt");
 
 	Grid grid(m, vector<int>(n, 0));
 
-	string s;
-	while (getline(input, s)) {
-		string s_copy = s;
-
-		// coords for begin and end point
+	string line;
+	while (getline(input, line)) {
+		stringstream ss(line);
+		string command, ignore;
 		Coordinate begin, end;
-		
-		begin.first = getInteger(s_copy);
-		begin.second = getInteger(s_copy);
-		end.first = getInteger(s_copy);
-		end.second = getInteger(s_copy);
 
-		// turn light on
-		if (s.find("turn on") != string::npos) {
-			action(grid, begin, end, "turn on");
+		ss >> command;
+		if (command == "turn") {
+			ss >> command;
 		}
-		// turn light off
-		else if (s.find("turn off") != string::npos) {
-			action(grid, begin, end, "turn off");
-		}
-		// turn light on
-		else if (s.find("toggle") != string::npos) {
-			action(grid, begin, end, "toggle");
-		}
+		
+		ss >> begin.first;
+		ss.ignore(1, ',');
+		ss >> begin.second >> ignore >> end.first;
+		ss.ignore(1, ',');
+		ss >> end.second;
+
+		action(grid, begin, end, command);
 	}
 
 	input.close();
 
-	result = countTrueValues(grid);
-
-	cout << result << endl;
+	cout << countTrueValues(grid) << endl;
 
 	return 0;
 }
