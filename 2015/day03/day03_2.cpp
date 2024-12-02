17e51a7278dbfa0de779123b7cc164ddbff8b439
Min Hee Jo <roxy2131@gmail.com>
2024-12-02 22:40:05 +0100

    Optimize: 2015 day 3 part 2

--- 2015/day03/day03_2.cpp
+++ 2015/day03/day03_2.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <map>
+#include <set>
 #include <utility>
 
 using namespace std;
 
 typedef pair<int, int> grid;
-typedef map<grid, bool> grid_bool;
 
 int main() {
-	int result = 0;
 	ifstream input("input.txt");
 
 	grid start = {0, 0};
@@ -19,42 +17,28 @@ int main() {
 	// false -> Santa, true -> Robot
 	bool turn = false;
 
-	grid_bool visited;
-	visited.insert({start, true});
+	set<grid> visited;
+	visited.insert(start);
 
 	char c;
 	while (input.get(c)) {
-		// santa 
-		grid* pos = &posSanta;
-		// robot
-		if (turn) {
-			pos = &posRobot;	
-		}
+		grid& currentPos = turn ? posRobot : posSanta;
 
+		switch (c) {
+            case '<': currentPos.first--; break;
+            case '>': currentPos.first++; break;
+            case '^': currentPos.second++; break;
+            case 'v': currentPos.second--; break;
+        }
+
+		visited.insert(currentPos);
 		// take turns
 		turn = !turn;
-
-		if (c == '<') {
-			pos->first--;
-		}
-		else if (c == '>') {
-			pos->first++;
-		}
-		else if (c == 'v') {
-			pos->second--;
-		}
-		else if (c == '^') {
-			pos->second++;
-		}
-
-		visited.insert({*pos, true});
 	}
 
 	input.close();
 
-	result = visited.size();
-
-	cout << result << endl;
+	cout << visited.size() << endl;
 	
 	return 0;
 }
