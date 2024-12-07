69844d92aa70b4019ff54cf381a05f244c101a6b
Min Hee Jo <roxy2131@gmail.com>
2024-12-08 00:25:24 +0100

    Solved: 2024 day 7 part 1

---
+++ 2024/day07/day07_1.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+bool foundResult(const vector<int>& numbers, const long long int& result, const long long int& cur_result, const int& index) {
+    if (numbers.size() == 1) {
+        if (cur_result == result)
+            return true;
+        else
+            return false;
+    }
+    else if (index == numbers.size()) {
+        if (cur_result == result)
+            return true;
+        else
+            return false;
+    }
+
+    // plus
+    bool plus_check = foundResult(numbers, result, cur_result + numbers[index], index + 1);
+
+    // multiply
+    bool multiply_check = foundResult(numbers, result, cur_result * numbers[index], index + 1);
+
+    return plus_check || multiply_check;
+}
+
+int main() {
+    long long int answer = 0;
+
+    ifstream input("input.txt");
+
+    string line;
+    while (getline(input, line)) {
+        istringstream iss(line);
+        long long int result;
+        int num;
+        char ignore;
+
+        iss >> result >> ignore;
+
+        vector<int> numbers;
+
+        while (iss >> num) {
+            numbers.push_back(num);
+        }
+
+        if(foundResult(numbers, result, numbers[0], 1)) {
+            answer += result;
+        }
+    }
+
+    cout << answer << endl;
+
+    return 0;
+}
